4a360cc7524228c250646e47de2f21bd2abf3af8
Renan Birck Pinheiro <renan.birck.pinheiro@gmail.com>
2021-03-08 14:45:26 -0300

    RAM read and write works.

--- cpu.py
+++ cpu.py
@@ -1,4 +1,17 @@
 # py6502: a 6502 emulator in Python.
 
 class CPU():
-    pass
+
+    def __init__(self, memory_size = 65536): # Inicialize a new CPU. 
+                                             # The default RAM size is 64 KiB.
+
+        self.PC = 0x0000  # Program counter
+        self.SP = 0x0000  # Stack pointer
+        self.A, self.X, self.Y = 0x00, 0x00, 0x00 # Registers
+        self.RAM = [0x00] * memory_size
+
+    def read_RAM(self, address):
+        return self.RAM[address]
+
+    def write_RAM(self, address, value):
+        self.RAM[address] = value & 0xFF  # Limit to 1 byte
--- test_CPU.py
+++ test_CPU.py
@@ -1,5 +1,18 @@
+import pytest
 from cpu import CPU
 
-def test_CPU():
+
+class TestCPU():
+
+    cpu_under_test = CPU()
+
+    def test_read_RAM_outside_bounds(self):
+        with pytest.raises(IndexError):
+            self.cpu_under_test.read_RAM(0xFFFFFFFF)
+
+    def test_read_and_write_RAM(self):
+        for i in range(0, 0x10000):
+            self.cpu_under_test.write_RAM(i, i & 0xFF) # Get the lowest byte
+            assert self.cpu_under_test.read_RAM(i) == (i & 0xFF)
+
     pass
-    
