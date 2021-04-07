7472a944a4218ac09829a110cba68576afdfceb0
Renan Birck Pinheiro <renan.birck.pinheiro@gmail.com>
2021-04-07 15:30:28 -0300

    Addressing modes finished.

--- cpu.py
+++ cpu.py
@@ -28,18 +28,18 @@ class AddressingMode(Enum):
     INDIRECT = auto()
     INDIRECT_X = auto()
     INDIRECT_Y = auto()
-  
 
 class CPU():
-    def __init__(self, memory_size = 65536): # Inicialize a new CPU. 
-                                             # The default RAM size is 64 KiB.
+    def __init__(self, memory_size = 65536): # Inicialize a new CPU.
+                                   # The default RAM size is 64 KiB.
+        
         self.ticks = 0    # Tick count
 
         # When is executed an instruction, this callback will
         # be called as needed. It receives self, thus it has access
         # to everything that has happening within the CPU.
 
-        self.callback_on_instruction = None 
+        self.callback_on_instruction = None
         self.PC = 0x0000  # Program counter
         self.SP = 0x0000  # Stack pointer
         self.A, self.X, self.Y = 0x00, 0x00, 0x00 # Registers
@@ -53,7 +53,7 @@ class CPU():
     def write_RAM(self, address, value):
         print(f"RAM[0x{address:02X}] <- 0x{value:04X}")
         self.RAM[address] = value & 0xFF  # Limit to 1 byte
-    
+
     def reset_CPU(self):
         logging.info("CPU reset.")
         self.A, self.X, self.Y = 0x00, 0x00, 0x00
@@ -69,7 +69,7 @@ class CPU():
     def push_8bit(self, value):
         self.write_RAM(self.STACK_BASE + self.SP, value)
         self.SP -= 1
-    
+
     def pop_8bit(self):
         self.SP += 1
         return self.read_RAM(self.STACK_BASE + self.SP)
@@ -83,7 +83,7 @@ class CPU():
     #    self.write_RAM(self.STACK_BASE + self.SP, value_high)
     #    self.write_RAM(self.STACK_BASE + ((self.SP - 1) & 0xFF), value_low)
     #    self.SP -= 2
-    
+
     def pop_16bit(self):
         value_low = self.pop_8bit()
         value_high = self.pop_8bit()
@@ -96,11 +96,11 @@ class CPU():
 
     def find_addressing_mode(self, opcode):
         # Given opcode, find its addressing mode.
-        
+
         opcode_high = (opcode & 0xF0)>>4
         opcode_low = opcode & 0x0F
-        
-        if opcode == 0x20 or (opcode != 0x6C and opcode_high in [0x00, 0x02, 0x04, 0x06, 0x08, 0x0A, 0x0C, 0x0E] 
+
+        if opcode == 0x20 or (opcode != 0x6C and opcode_high in [0x00, 0x02, 0x04, 0x06, 0x08, 0x0A, 0x0C, 0x0E]
                                              and opcode_low  in [0x0C, 0x0D, 0x0E, 0x0F]):
             return AddressingMode.ABSOLUTE
         elif (opcode_high in [0x1, 0x3, 0x5, 0x7, 0xD, 0xF] and opcode_low in [0x0C, 0x0D, 0x0E, 0x0F]) or (opcode in [0x9C, 0x9D, 0xBC, 0xBD]):
@@ -110,20 +110,33 @@ class CPU():
             return AddressingMode.ABSOLUTE_Y
         elif opcode_high in [0x0, 0x2, 0x4, 0x6] and opcode_low == 0xA:
             return AddressingMode.ACCUMULATOR
-
+        elif (opcode_high in [0x0, 0x2, 0x4, 0x6] and opcode_low in [0x9, 0xB]) \
+        or (opcode_high in [0x8, 0xA, 0xC, 0xE] and opcode_low in [0x0, 0x2, 0x9, 0xB]):
+            return AddressingMode.IMMEDIATE
+        elif (opcode_high in [0x0, 0x2, 0x4, 0x6, 0x8, 0xA, 0xC, 0xE] and opcode_low in [0x1, 0x3]):
+            return AddressingMode.INDIRECT_X
+        elif (opcode_high in [0x1, 0x3, 0x5, 0x7, 0x9, 0xB, 0xD, 0xF] and opcode_low in [0x1, 0x3]):
+            return AddressingMode.INDIRECT_Y
+        elif (opcode_high in [0x1, 0x3, 0x5, 0x7, 0x9, 0xB, 0xD, 0xF] and opcode_low == 0x00):
+            return AddressingMode.RELATIVE
+        elif (opcode_high in [0x0, 0x2, 0x4, 0x6, 0x8, 0xA, 0xC, 0xE] and opcode_low in [0x4, 0x5, 0x6, 0x7]):
+            return AddressingMode.ZERO_PAGE
+        elif (opcode_high in [0x1, 0x3, 0x5, 0x7, 0xD, 0xF] and opcode_low in [0x4, 0x5, 0x6, 0x7]) \
+        or (opcode_high in [0x9, 0xB] and opcode_low in [0x4, 0x5]):
+            return AddressingMode.ZERO_PAGE_X
+        elif opcode in [0x96, 0x97, 0xB6, 0xB7]:
+            return AddressingMode.ZERO_PAGE_Y
+        
         # The default (most common) is implied addressing.
         return AddressingMode.IMPLIED
         
     def step(self):
         # Fetch
-        
         opcode = self.read_RAM(self.PC)
         print(f"FETCH RAM[0x{self.PC:02X}] = 0x{opcode:02X}")
-
         self.PC += 1
-        
-        # Decode 
 
+        # Decode 
         opcode_high = (opcode & 0xF0)>>4
         opcode_low = opcode & 0x0F
 
@@ -135,20 +148,17 @@ class CPU():
         else:
             raise NotImplementedError(f"Instruction 0x{opcode:02X} not yet implemented!")        # Execute
     
-
-    
     ## The instructions themselves
-
     def BRK(self):
         self.PC += 1
         # TODO: handle the stack
         self.push_16bit(self.PC)
         # Read the new PC from the BRK vector
         self.PC = (self.read_RAM(0xFFFF)) << 8 | self.read_RAM(0xFFFE)
-    
+
     def CLC(self):
         self.STATUS &= ~StatusRegister.CARRY
-    
+
     def CLD(self):
         self.STATUS &= ~StatusRegister.DECIMAL
 
