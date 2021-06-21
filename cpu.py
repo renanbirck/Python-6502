63f6ddc66d813b4af21c193fb27e1036d1d8a26b
Renan Birck Pinheiro <renan.birck.pinheiro@gmail.com>
2021-06-21 16:03:51 -0300

    Absolute addressing mode.

--- cpu.py
+++ cpu.py
@@ -18,7 +18,7 @@ class StatusRegister(Flag):
     CARRY      = 0x01
     NOTHING    = 0x00
 
-class AddressingMode(Enum):
+class AddressingModes(Enum):
     """ The possible addressing modes. """
     IMPLIED = auto()
     ACCUMULATOR = auto()
@@ -102,47 +102,47 @@ class CPU():
                       0x2F, 0x4C, 0x4D, 0x4E, 0x4F, 0x6D, 0x6E, 0x6F,
                       0x8C, 0x8D, 0x8E, 0x8F, 0xAC, 0xAD, 0xAE, 0xAF,
                       0xCC, 0xCD, 0xCE, 0xCF, 0xEC, 0xED, 0xEE, 0xEF]:
-            return AddressingMode.ABSOLUTE
+            return AddressingModes.ABSOLUTE
         elif opcode in [0x1C, 0x1D, 0x1E, 0x1F, 0x3C, 0x3D, 0x3E, 0x3F,
                         0x5C, 0x5D, 0x5E, 0x5F, 0x7C, 0x7D, 0x7E, 0x7F,
                         0x9C, 0x9D, 0xBC, 0xBD, 0xDC, 0xDD, 0xDE, 0xDF,
                         0xFC, 0xFD, 0xFE, 0xFF]:
-            return AddressingMode.ABSOLUTE_X
+            return AddressingModes.ABSOLUTE_X
         elif opcode in [0x19, 0x1B, 0x39, 0x3B, 0x59, 0x5B, 0x79, 0x7B,
                         0x99, 0x9B, 0x9E, 0x9F, 0xB9, 0xBB, 0xBE, 0xBF,
                         0xD9, 0xDB, 0xF9, 0xFB]:
-            return AddressingMode.ABSOLUTE_Y
+            return AddressingModes.ABSOLUTE_Y
         elif opcode in [0x0A, 0x2A, 0x4A, 0x6A]:
-            return AddressingMode.ACCUMULATOR
+            return AddressingModes.ACCUMULATOR
         elif opcode in [0x09, 0x0B, 0x29, 0x2B, 0x49, 0x4B, 0x69, 0x6B,
                         0x80, 0x82, 0x89, 0x8B, 0xA0, 0xA2, 0xA9, 0xAB,
                         0xC0, 0xC2, 0xC9, 0xCB, 0xE0, 0xE2, 0xE9, 0xEB]:
-            return AddressingMode.IMMEDIATE
+            return AddressingModes.IMMEDIATE
         elif opcode == 0x6C:
-            return AddressingMode.INDIRECT
+            return AddressingModes.INDIRECT
         elif opcode in [0x01, 0x03, 0x21, 0x23, 0x41, 0x43, 0x61, 0x63,
                         0x81, 0x83, 0xA1, 0xA3, 0xC1, 0xC3, 0xE1, 0xE3]:
-            return AddressingMode.INDIRECT_X
+            return AddressingModes.INDIRECT_X
         elif opcode in [0x11, 0x13, 0x31, 0x33, 0x51, 0x53, 0x71, 0x73,
                         0x91, 0x93, 0xB1, 0xB3, 0xD1, 0xD3, 0xF1, 0xF3]:
-            return AddressingMode.INDIRECT_Y
+            return AddressingModes.INDIRECT_Y
         elif opcode in [0x10, 0x30, 0x50, 0x70, 0x90, 0xB0, 0xD0, 0xF0]:
-            return AddressingMode.RELATIVE
+            return AddressingModes.RELATIVE
         elif opcode in [0x04, 0x05, 0x06, 0x07, 0x24, 0x25, 0x26, 0x27,
                         0x44, 0x45, 0x46, 0x47, 0x64, 0x65, 0x66, 0x67,
                         0x84, 0x85, 0x86, 0x87, 0xA4, 0xA5, 0xA6, 0xA7,
                         0xC4, 0xC5, 0xC6, 0xC7, 0xE4, 0xE5, 0xE6, 0xE7]:
-            return AddressingMode.ZERO_PAGE
+            return AddressingModes.ZERO_PAGE
         elif opcode in [0x14, 0x15, 0x16, 0x17, 0x34, 0x35, 0x36, 0x37,
                         0x54, 0x55, 0x56, 0x57, 0x74, 0x75, 0x76, 0x77,
                         0x94, 0x95, 0xB4, 0xB5, 0xD4, 0xD5, 0xD6, 0xD7,
                         0xF4, 0xF5, 0xF6, 0xF7]:
-            return AddressingMode.ZERO_PAGE_X
+            return AddressingModes.ZERO_PAGE_X
         elif opcode in [0x96, 0x97, 0xB6, 0xB7]:
-            return AddressingMode.ZERO_PAGE_Y
+            return AddressingModes.ZERO_PAGE_Y
 
         # The default (most common) is implied addressing.
-        return AddressingMode.IMPLIED
+        return AddressingModes.IMPLIED
 
     def find_instruction_cost(self, opcode):
         # Given an opcode, it returns how many ticks this opcode costs.
@@ -204,21 +204,32 @@ class CPU():
         return (instruction, addressing_mode, cost)
 
     def compute_effective_address(self, addressing_mode):
-        if addressing_mode in [AddressingMode.ACCUMULATOR,
-                               AddressingMode.IMPLIED]:
+        if addressing_mode in [AddressingModes.ACCUMULATOR,
+                               AddressingModes.IMPLIED]:
             return
 
         else:
             self.PC += 1
-            if addressing_mode == AddressingMode.IMMEDIATE:
+            if addressing_mode == AddressingModes.IMMEDIATE:
                 self.EA = self.PC
-            elif addressing_mode == AddressingMode.ZERO_PAGE:
+            elif addressing_mode == AddressingModes.ZERO_PAGE:
                 self.EA = self.read_RAM(self.PC) # EA <- RAM[PC]
                 ### Since 0 <= PC <= 0xFF... reads from zero-page
-            elif addressing_mode == AddressingMode.ZERO_PAGE_X:
+            elif addressing_mode == AddressingModes.ZERO_PAGE_X:
                 self.EA = self.read_RAM((self.PC + self.X) & 0xFF)
-            elif addressing_mode == AddressingMode.ZERO_PAGE_Y:
+            elif addressing_mode == AddressingModes.ZERO_PAGE_Y:
                 self.EA = self.read_RAM((self.PC + self.Y) & 0xFF)
+            elif addressing_mode == AddressingModes.RELATIVE:
+                # Relative addressing mode is slightly different...
+                # we will use a different variable name.
+                self.RA = self.read_RAM(self.PC)
+                if (self.RA & 0x80):  # It's > 127...
+                    self.RA |= 0xFF00 ## so it wraps back.
+            elif addressing_mode == AddressingModes.ABSOLUTE:
+                EA_low = self.read_RAM(self.PC - 1)
+                EA_high = self.read_RAM(self.PC)
+                self.EA = (EA_high << 8) | EA_low
+                        
             
     def step(self):
         # Fetch
--- test_CPU.py
+++ test_CPU.py
@@ -118,63 +118,63 @@ class TestCPU():
                         0x8E, 0x8F, 0xAC, 0xAD, 0xAE, 0xAF,
                         0xCC, 0xCD, 0xCE, 0xCF, 0xEC, 0xED,
                         0xEE, 0xEF]:
-            assert self.cpu_under_test.find_addressing_mode(opcode) == cpu.AddressingMode.ABSOLUTE
+            assert self.cpu_under_test.find_addressing_mode(opcode) == cpu.AddressingModes.ABSOLUTE
         for opcode in [0x1C, 0x1D, 0x1E, 0x1F, 0x3C, 0x3D, 0x3E,
                        0x3F, 0x5C, 0x5D, 0x5E, 0x5F, 0x7C, 0x7D,
                        0x7E, 0x7F, 0x9C, 0x9D, 0xBC, 0xBD, 0xDC,
                        0xDD, 0xDE, 0xDF, 0xFC, 0xFD, 0xFE, 0xFF]:
-            assert self.cpu_under_test.find_addressing_mode(opcode) == cpu.AddressingMode.ABSOLUTE_X
+            assert self.cpu_under_test.find_addressing_mode(opcode) == cpu.AddressingModes.ABSOLUTE_X
         for opcode in [0x19, 0x1B, 0x39, 0x3B, 0x59,
                        0x5B, 0x79, 0x7B, 0x99, 0x9B,
                        0x9E, 0x9F, 0xB9, 0xBB, 0xBE,
                        0xBF, 0xD9, 0xDB, 0xF9, 0xFB]:
-            assert self.cpu_under_test.find_addressing_mode(opcode) == cpu.AddressingMode.ABSOLUTE_Y
+            assert self.cpu_under_test.find_addressing_mode(opcode) == cpu.AddressingModes.ABSOLUTE_Y
         for opcode in [0x0A, 0x2A, 0x4A, 0x6A]:
-            assert self.cpu_under_test.find_addressing_mode(opcode) == cpu.AddressingMode.ACCUMULATOR
+            assert self.cpu_under_test.find_addressing_mode(opcode) == cpu.AddressingModes.ACCUMULATOR
         for opcode in [0x09, 0x0B, 0x29, 0x2B, 0x49,
                        0x4B, 0x69, 0x6B, 0x80, 0x82,
                        0x89, 0x8B, 0xA0, 0xA2, 0xA9,
                        0xAB, 0xC0, 0xC2, 0xC9, 0xCB,
                        0xE0, 0xE2, 0xE9, 0xEB]:
-            assert self.cpu_under_test.find_addressing_mode(opcode) == cpu.AddressingMode.IMMEDIATE
+            assert self.cpu_under_test.find_addressing_mode(opcode) == cpu.AddressingModes.IMMEDIATE
         for opcode in [0x01, 0x03, 0x21, 0x23, 0x41,
                        0x43, 0x61, 0x63, 0x81, 0x83,
                        0xA1, 0xA3, 0xC1, 0xC3, 0xE1,
                        0xE3]:
-            assert self.cpu_under_test.find_addressing_mode(opcode) == cpu.AddressingMode.INDIRECT_X
+            assert self.cpu_under_test.find_addressing_mode(opcode) == cpu.AddressingModes.INDIRECT_X
         for opcode in [0x11, 0x13, 0x31, 0x33, 0x51,
                        0x53, 0x71, 0x73, 0x91, 0x93,
                        0xB1, 0xB3, 0xD1, 0xD3, 0xF1,
                        0xF3]:
-            assert self.cpu_under_test.find_addressing_mode(opcode) == cpu.AddressingMode.INDIRECT_Y
+            assert self.cpu_under_test.find_addressing_mode(opcode) == cpu.AddressingModes.INDIRECT_Y
         for opcode in [0x10, 0x30, 0x50, 0x70,
                        0x90, 0xB0, 0xD0, 0xF0]:
-            assert self.cpu_under_test.find_addressing_mode(opcode) == cpu.AddressingMode.RELATIVE
+            assert self.cpu_under_test.find_addressing_mode(opcode) == cpu.AddressingModes.RELATIVE
         for opcode in [0x04, 0x05, 0x06, 0x07, 0x24, 0x25, 0x26, 0x27,
                        0x44, 0x45, 0x46, 0x47, 0x64, 0x65, 0x66, 0x67,
                        0x84, 0x85, 0x86, 0x87, 0xA4, 0xA5, 0xA6, 0xA7,
                        0xC4, 0xC5, 0xC6, 0xC7, 0xE4, 0xE5, 0xE6, 0xE7]:
-            assert self.cpu_under_test.find_addressing_mode(opcode) == cpu.AddressingMode.ZERO_PAGE
+            assert self.cpu_under_test.find_addressing_mode(opcode) == cpu.AddressingModes.ZERO_PAGE
         for opcode in [0x14, 0x15, 0x16, 0x17, 0x34, 0x35, 0x36, 0x37,
                        0x54, 0x55, 0x56, 0x57, 0x74, 0x75, 0x76, 0x77,
                        0x94, 0x95, 0xB4, 0xB5, 0xD4, 0xD5, 0xD6, 0xD7,
                        0xF4, 0xF5, 0xF6, 0xF7]:
-            assert self.cpu_under_test.find_addressing_mode(opcode) == cpu.AddressingMode.ZERO_PAGE_X
+            assert self.cpu_under_test.find_addressing_mode(opcode) == cpu.AddressingModes.ZERO_PAGE_X
         for opcode in [0x96, 0x97, 0xB6, 0xB7]:
-            assert self.cpu_under_test.find_addressing_mode(opcode) == cpu.AddressingMode.ZERO_PAGE_Y
-        assert self.cpu_under_test.find_addressing_mode(0x00) == cpu.AddressingMode.IMPLIED
+            assert self.cpu_under_test.find_addressing_mode(opcode) == cpu.AddressingModes.ZERO_PAGE_Y
+        assert self.cpu_under_test.find_addressing_mode(0x00) == cpu.AddressingModes.IMPLIED
 
     def test_decode_instruction(self):
         opcode = 0x80
         instruction, addressing_mode, cost = self.cpu_under_test.decode_instruction(opcode)
         assert instruction == self.cpu_under_test.NOP
-        assert addressing_mode == cpu.AddressingMode.IMMEDIATE
+        assert addressing_mode == cpu.AddressingModes.IMMEDIATE
         assert cost == 2
 
         opcode = 0x18 # CLC
         instruction, addressing_mode, cost = self.cpu_under_test.decode_instruction(opcode)
         assert instruction == self.cpu_under_test.CLC
-        assert addressing_mode == cpu.AddressingMode.IMPLIED
+        assert addressing_mode == cpu.AddressingModes.IMPLIED
         assert cost == 2
     
     # imp, acc don't change ea, so they are not tested.
@@ -182,7 +182,7 @@ class TestCPU():
         # Immediate. EA <- PC+1
         PC_old = self.cpu_under_test.PC
 
-        self.cpu_under_test.compute_effective_address(cpu.AddressingMode.IMMEDIATE)
+        self.cpu_under_test.compute_effective_address(cpu.AddressingModes.IMMEDIATE)
         assert self.cpu_under_test.EA == PC_old + 1
 
     def test_find_effective_address_zp(self):
@@ -194,7 +194,7 @@ class TestCPU():
             self.cpu_under_test.write_RAM(position, value)
         
         self.cpu_under_test.PC = 0x4000  # PC <- 0x4000
-        self.cpu_under_test.compute_effective_address(cpu.AddressingMode.ZERO_PAGE) # EA <- RAM[PC+1]
+        self.cpu_under_test.compute_effective_address(cpu.AddressingModes.ZERO_PAGE) # EA <- RAM[PC+1]
         assert self.cpu_under_test.EA == self.cpu_under_test.read_RAM(0x4001) # .:. EA <- RAM[0x4001]
      
     def test_find_effective_address_zpx(self):
@@ -208,7 +208,7 @@ class TestCPU():
         # The target address we want: PC + 1 + X
         target_EA = self.cpu_under_test.PC + self.cpu_under_test.X + 1 
 
-        self.cpu_under_test.compute_effective_address(cpu.AddressingMode.ZERO_PAGE_X) # EA <- RAM[PC+1+X]
+        self.cpu_under_test.compute_effective_address(cpu.AddressingModes.ZERO_PAGE_X) # EA <- RAM[PC+1+X]
         assert self.cpu_under_test.EA == self.cpu_under_test.read_RAM(target_EA)
     
     def test_find_effective_address_zpy(self):
@@ -222,10 +222,22 @@ class TestCPU():
         # The target address we want: PC + 1 + Y
         target_EA = self.cpu_under_test.PC + self.cpu_under_test.Y + 1 
 
-        self.cpu_under_test.compute_effective_address(cpu.AddressingMode.ZERO_PAGE_Y) # EA <- RAM[PC+1+X]
+        self.cpu_under_test.compute_effective_address(cpu.AddressingModes.ZERO_PAGE_Y) # EA <- RAM[PC+1+X]
         assert self.cpu_under_test.EA == self.cpu_under_test.read_RAM(target_EA)
     
+    def test_find_effective_address_abs(self):
+        # Absolute mode. EA <- RAM[PC, PC+1]    
+        for position in range(0, 65536):
+            value = position & 0xFF
+            self.cpu_under_test.write_RAM(position, value)
         
+        self.cpu_under_test.PC = 0x4040
+        target_EA_low = self.cpu_under_test.read_RAM(self.cpu_under_test.PC)
+        target_EA_high = self.cpu_under_test.read_RAM(self.cpu_under_test.PC + 1)
+        target_EA = (target_EA_high << 8) | target_EA_low
+        
+        self.cpu_under_test.compute_effective_address(cpu.AddressingModes.ABSOLUTE)
+        assert self.cpu_under_test.EA == target_EA
         
     ##### INSTRUCTION TESTS
 
